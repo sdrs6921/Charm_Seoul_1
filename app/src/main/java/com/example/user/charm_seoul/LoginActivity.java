8e0e9929fd1dad83b4016c8b2daab48b766dac57
sdrs6921 <sdrs6921@naver.com>
2018-08-23 17:49:06 +0900

    facebook login 후 메인액티비티로 넘어가기

--- app/src/main/java/com/example/user/charm_seoul/LoginActivity.java
+++ app/src/main/java/com/example/user/charm_seoul/LoginActivity.java
@@ -57,62 +57,9 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-
-
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
-
-    /*
-    public void facebookLoginOnClick(View v){
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        //callbackManager = CallbackManager.Factory.create();
-
-        LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this,
-                Arrays.asList("public_profile", "email"));
-        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-
-            @Override
-            public void onSuccess(final LoginResult result) {
-
-                GraphRequest request;
-                request = GraphRequest.newMeRequest(result.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
-
-                    @Override
-                    public void onCompleted(JSONObject user, GraphResponse response) {
-                        if (response.getError() != null) {
-
-                        } else {
-                            Log.i("TAG", "user: " + user.toString());
-                            Log.i("TAG", "AccessToken: " + result.getAccessToken().getToken());
-                            setResult(RESULT_OK);
-
-                            Intent i = new Intent(LoginActivity.this, MainActivity.class);
-                            startActivity(i);
-                            finish();
-                        }
-                    }
-                });
-                Bundle parameters = new Bundle();
-                parameters.putString("fields", "id,name,email,gender,birthday");
-                request.setParameters(parameters);
-                request.executeAsync();
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-                Log.e("test", "Error: " + error);
-                //finish();
-            }
-
-            @Override
-            public void onCancel() {
-                //finish();
-            }
-        });
-    }
-*/
 }
